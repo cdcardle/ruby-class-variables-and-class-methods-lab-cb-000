295299d461005338cf375a583e571d03396be42d
Chris Cardle <cdcardle@gmail.com>
2017-11-08 23:14:14 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,12 +1,12 @@
 class Song
   attr_accessor :name, :artist, :genre
-  @@count = []
+  @@count = 0
   @@artists = []
   @@genres = []
 
   def initialize(name, artist, genre)
     @name = name; @artist = artist; @genre = genre;
-
+    @@count += 1
   end
 
   def method_name
