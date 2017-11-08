dab42b983e4ccd7919dd4ca810abd9a1abe78134
Chris Cardle <cdcardle@gmail.com>
2017-11-08 23:15:27 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -7,6 +7,8 @@ class Song
   def initialize(name, artist, genre)
     @name = name; @artist = artist; @genre = genre;
     @@count += 1
+    @@artists << name unless @@artists.include?(artist)
+    @@genres << name unless @@genres.include?(genre)
   end
 
   def method_name
